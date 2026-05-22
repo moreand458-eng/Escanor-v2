89c1a6b08eef10c0a7b1013c02585fb6966fd070
𝑬𝑺𝑪𝑨𝑵𝑶𝑹 <moreand458@gmail.com>
2026-05-22 16:01:36 +0300

    Add files via upload

---
+++ plugins/group/script.js
@@ -0,0 +1,40 @@
+let handler = async (m, {
+    conn,
+    bot
+}) => {
+const context = (jid, img) => ({
+    mentionedJid: [jid],
+    isForwarded: true,
+    forwardingScore: 1,
+    forwardedNewsletterMessageInfo: {
+        newsletterJid: '120363422581600030@newsletter',
+        newsletterName: '𝐄𝐒𝟏 ~ 𝐂𝐡𝐚𝐧𝐧𝐞𝐥 🕷️',
+        serverMessageId: 0
+    },
+    externalAdReply: {
+        title: "𝑬𝑺𝑪𝑨𝑵𝑶𝑹 𝑩𝑶𝑻👨🏻‍💻🔥 | 𝐁𝐨𝐭 𝐢𝐬 𝐛𝐮𝐢𝐥𝐭 𝐨𝐧 𝐭𝐡𝐞 𝐄𝐒/𝐄𝐒𝐂 𝐟𝐫𝐚𝐦𝐞𝐰𝐨𝐫𝐤",
+        body: "𝚆𝚑𝚊𝚝𝚜𝙰𝚙𝚙 𝚋𝚘𝚝 𝚝𝚑𝚊𝚝 𝚒𝚜 𝚎𝚊𝚜𝚢 𝚝𝚘 𝚖𝚘𝚍𝚒𝚏𝚢 𝚊𝚗𝚍 𝚟𝚎𝚛𝚢 𝚏𝚊𝚜𝚝",
+        thumbnailUrl: img,
+        sourceUrl: '',
+        mediaType: 1,
+        renderLargerThumbnail: true
+    }
+});
+const { images } = bot.config.info;
+const img = images.random()
+await conn.sendMessage(m.chat, { 
+  text: `
+GitHub: _*https://github.com/moreand458-eng/Escanor-bot/tree/main*_
+
+Video: _*https://youtube.com/@escanor_soft-1?si=NQXfvUay8ZvzBBzB*_
+
+> *لا تنسي وضع نجمة لـ الريبو 🌟*
+`,
+  contextInfo: context(m.sender, img)
+}, { quoted: reply_status });
+}
+handler.usage = ["سكريبت"];
+handler.category = "group";
+handler.command = ["سكريبت", "سورس", "sc"];
+
+export default handler;
