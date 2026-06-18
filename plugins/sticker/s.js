cde44a14ff1136f5aa9a496d4942e2413d8bb7af
𝑬𝑺𝑪𝑨𝑵𝑶𝑹 <moreand458@gmail.com>
2026-06-18 10:22:53 +0300

    Add files via upload

--- plugins/sticker/s.js
+++ plugins/sticker/s.js
@@ -2,16 +2,19 @@ import { createSticker } from "../../system/utils.js";
 
 const test = async (m, { conn, bot }) => {
   if (!m.quoted) return m.reply("❤️ ~ يرجى الرد على صورة أو فيديو لتحويله إلى ملصق ~ 💙");
-  
-const { pack, author } = bot.config.info.copyright
-const q = await m.quoted;
-const buffer = await createSticker(await q.download(), { mime: q.mimetype, pack, author })
 
-await conn.sendMessage(
-  m.chat,
-  { sticker: buffer },
-  { quoted: reply_status }
-);
+  const { pack, author } = bot.config.info.copyright;
+  const q = await m.quoted;
+
+  if (!q || !q.mimetype) return m.reply("❌ تعذّر قراءة الميديا، تأكد أنك رددت على صورة أو فيديو صحيح.");
+
+  const buffer = await createSticker(await q.download(), { mime: q.mimetype, pack, author });
+
+  await conn.sendMessage(
+    m.chat,
+    { sticker: buffer },
+    { quoted: global.reply_status || m }
+  );
 };
 
 test.usage = ["ملصق"];
